050c545a16458d2a72f3a6b9a94be0a9fa5a6412
ehnry055 <henry.g.choi@gmail.com>
2024-11-20 23:41:17 -0500

    react, bootstrap setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JS
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="my-4">Hello, Henry! jaja</h1>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+};
+
+export default App;
