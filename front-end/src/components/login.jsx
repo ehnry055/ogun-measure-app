c48c2d38edf5624bff4b0d8bf14eedf40fe512f6
ehnry055 <henry.g.choi@gmail.com>
2024-12-02 18:11:48 -0500

    login basic no funcitonality

--- front-end/src/components/login.jsx
+++ front-end/src/components/login.jsx
@@ -2,15 +2,9 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 const LoginButton = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
-  return (
-    !isAuthenticated && (
-      <button onClick={() => loginWithRedirect()}>
-       Log In
-      </button>
-    )
-  )
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
 };
 
 export default LoginButton;
