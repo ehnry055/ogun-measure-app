33fcf91a4dd3572b146e304bd0c80c93ef2a5667
ehnry055 <henry.g.choi@gmail.com>
2024-12-02 01:00:21 -0500

    small changes

--- src/App.js
+++ src/App.js
@@ -7,22 +7,73 @@ import HighchartsReact from 'highcharts-react-official';
 
 const options = {
   chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
     type: 'pie'
-  },
-  title: {
-    text: 'charts test'
-  },
-  series: [
-    {
-      data: [1, 2, 1, 4, 3, 6]
+},
+title: {
+    text: 'Browser market shares in May, 2020',
+    align: 'left'
+},
+tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+},
+accessibility: {
+    point: {
+        valueSuffix: '%'
     }
-  ]
+},
+plotOptions: {
+    pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+        }
+    }
+},
+series: [{
+    name: 'Brands',
+    colorByPoint: true,
+    data: [{
+        name: 'Chrome',
+        y: 70.67,
+        sliced: true,
+        selected: true
+    }, {
+        name: 'Edge',
+        y: 14.77
+    },  {
+        name: 'Firefox',
+        y: 4.86
+    }, {
+        name: 'Safari',
+        y: 2.63
+    }, {
+        name: 'Internet Explorer',
+        y: 1.53
+    },  {
+        name: 'Opera',
+        y: 1.40
+    }, {
+        name: 'Sogou Explorer',
+        y: 0.84
+    }, {
+        name: 'QQ',
+        y: 0.51
+    }, {
+        name: 'Other',
+        y: 2.6
+    }]
+}]
 };
 
 const App = () => {
   return (
     <div className="container">
-      <h1 className="my-4">Hello, Henry! jaja</h1>
+      <h1 className="my-4">Hello, Henry! Data Visualization</h1>
       <button className="btn btn-primary">Click Me</button>
     
       <HighchartsReact highcharts={Highcharts} options={options} />
