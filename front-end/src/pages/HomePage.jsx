ab1b885211d74a9a75f5e50ee9a44c8aaf285a20
jchae0921 <jbro0921@gmail.com>
2024-12-16 01:44:10 -0500

    text

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -7,8 +7,8 @@ const HomePage = () => {
       <h2 className="about-title">About Us</h2>
       <div className="about-content">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna. 
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna.
+          Our goal is to provide an easy-to-use way of accessing a wealth of data about historial instances of racism throughout US counties. 
+          The data based on the Ogun measure, a method of capturing structural racism through 10 facets, 2 pathways, spanning across 3 historical periods. 
         </p>
       </div>
       <div className="about-grid">
