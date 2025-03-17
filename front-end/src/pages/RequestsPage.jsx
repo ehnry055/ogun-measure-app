e5a67a5e2c79dcd4e7a6423c82e369d72e5c85ab
ehnry055 <henry.g.choi@gmail.com>
2025-03-17 09:23:58 -0400

    adminview stop breaking

--- front-end/src/pages/RequestsPage.jsx
+++ front-end/src/pages/RequestsPage.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/App.css'; 
 
 const RequestsPage = () => {
+
   return (
     <div>
       <iframe
