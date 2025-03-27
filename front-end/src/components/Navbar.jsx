f1642feaab7e60deb11126a442ee348003af53e4
bwan31 <98164942+bwan31@users.noreply.github.com>
2025-03-26 22:06:04 -0400

    added some heurisitic stuff ui updates

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light navbar-gradient sticky-top">
       <div className="container-fluid">
-      <a class="navbar-brand" href="/">Racism Data System</a>
+      <a class="navbar-brand" href="/">Ogun Database</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
---
+++ front-end/src/pages/ItemDevelopers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/HomePage.css'; 
+
+function ItemDevelopers() {
+    return (
+        <div>
+            <div>tbc</div>
+        </div>
+    );
+};
+
+export default ItemDevelopers;
