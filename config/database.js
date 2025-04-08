765af88379996e9515533db5dc41ce4ff23e7731
Stephen Yoon <xbowmaster2020@gmail.com>
2025-04-08 03:22:58 -0400

    Fix migration and database config

--- config/database.js
+++ config/database.js
@@ -5,5 +5,12 @@ module.exports = {
   production: {
     use_env_variable: 'JAWSDB_URL',
     dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+    }
   }
+  
 };
