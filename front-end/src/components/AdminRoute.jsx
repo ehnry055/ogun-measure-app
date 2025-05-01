a44c104994c28caf4a65d91ff0843f203a258cda
ehnry055 <henry.g.choi@gmail.com>
2025-05-01 18:59:20 -0400

    troubleshooting adminroute

--- front-end/src/components/AdminRoute.jsx
+++ front-end/src/components/AdminRoute.jsx
@@ -3,12 +3,41 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { jwtDecode } from 'jwt-decode';
 import { useState, useEffect } from 'react';
 import UnAuthorized from '../pages/UnAuthorized';
+import { useNavigate } from 'react-router-dom';
 
 
 const AdminRoute = ({Component}) => {
     console.log("helllooo!!!");
     const { isAuthenticated, getAccessTokenSilently, error, isLoading } = useAuth0();
+    navigate = useNavigate();
+    const adminPerms = false;
 
+    useEffect(() => {
+        const checkPermissions = async () => {
+            try {
+
+                const token = await getAccessTokenSilently();
+                console.log("Access token:", token); // Log the token for debugging
+                const decodedToken = jwtDecode(token);
+                console.log("Decoded token:", decodedToken);
+        
+                const hasPermission = decodedToken.permissions && decodedToken.permissions.includes("adminView");
+                console.log("Has permission:", hasPermission);
+        
+                if (!hasPermission) {
+                console.log("User does not have the required permission");
+                navigate ("/unauthorized");
+                
+                }
+            } catch (error) {
+                console.error('Error checking permissions:', error);
+                navigate ("/unauthorized");
+            }
+        };
+    
+        checkPermissions();
+    }, [isAuthenticated, getAccessTokenSilently]);
+      
     if (isLoading) {
         console.log("loading...");
         return null;
@@ -16,30 +45,7 @@ const AdminRoute = ({Component}) => {
     if (!isAuthenticated && !isLoading) {
         return <UnAuthorized />;
     }
-    useEffect(() => {
-        const checkPermissions = async () => {
-          try {
-            const token = await getAccessTokenSilently();
-            console.log("Access token:", token); // Log the token for debugging
-            const decodedToken = jwtDecode(token);
-            console.log("Decoded token:", decodedToken);
-    
-            const hasPermission = decodedToken.permissions && decodedToken.permissions.includes("adminView");
-            console.log("Has permission:", hasPermission);
-    
-            if (!hasPermission) {
-              console.log("User does not have the required permission");
-              return <UnAuthorized />;
-            }
-          } catch (error) {
-            console.error('Error checking permissions:', error);
-            return <UnAuthorized />;
-          }
-        };
-    
-        checkPermissions();
-      }, [isAuthenticated, getAccessTokenSilently, navigate]);
-    
+
     return (
         <Component />
     );
