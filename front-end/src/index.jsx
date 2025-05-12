caa80ff58099d3ea10a7637655f7a6a8cf34a3a9
ehnry055 <henry.g.choi@gmail.com>
2025-05-12 11:50:39 -0400

    changed for localhost functionality over website

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -18,8 +18,8 @@ root.render(
       {
         audience: "https://ogun-measure-app-4fceb75a4928.herokuapp.com/api",
         //"racism-data-system.com/api",
-        redirect_uri: "https://ogun-measure-app-4fceb75a4928.herokuapp.com/"
-        //window.location.origin
+        redirect_uri: window.location.origin
+        //"https://ogun-measure-app-4fceb75a4928.herokuapp.com/"
       }
     }
     audience= "https://ogun-measure-app-4fceb75a4928.herokuapp.com/api"
