6fb5f7c2e5989e8fa2b699136673f4a6bbb698c4
Stephen Yoon <xbowmaster2020@gmail.com>
2025-05-15 02:23:49 -0400

    debugging previous commit

--- front-end/src/pages/ViewDatabasePage.jsx
+++ front-end/src/pages/ViewDatabasePage.jsx
@@ -15,33 +15,6 @@ const ViewDatabasePage = () => {
   const [presets, setPresets] = useState([]);
   const [selectedColumns, setSelectedColumns] = useState(new Set());
   const [selectedPreset, setSelectedPreset] = useState(null);
-
-  useEffect(() => {
-    const checkPermissions = async () => {
-      try {
-        const token = await getAccessTokenSilently();
-        console.log("Access token:", token); // Log the token for debugging
-        const decodedToken = jwtDecode(token);
-        console.log("Decoded token:", decodedToken);
-
-        const hasPermission = decodedToken.permissions && decodedToken.permissions.includes("adminView");
-        console.log("Has permission:", hasPermission);
-
-        if (!hasPermission) {
-          console.log("User does not have the required permission");
-          navigate("/unauthorized");
-        }
-        else {
-          setIsAuthorized(true);
-        }
-      } catch (error) {
-        console.error('Error checking permissions:', error);
-        navigate("/unauthorized");
-      }
-    };
-
-    checkPermissions();
-  }, [isAuthenticated, getAccessTokenSilently, navigate]);
   
   useEffect(() => {
     const savedPresets = localStorage.getItem('columnPresets');
