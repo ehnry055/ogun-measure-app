cb11cc6cb200904e81f701ab2d100580ecc4b3cd
ehnry055 <henry.g.choi@gmail.com>
2025-05-28 01:34:45 -0400

    react imports were wrong

--- front-end/src/components/profile.jsx
+++ front-end/src/components/profile.jsx
@@ -1,4 +1,5 @@
-import { useAuth0, useEffect, useState } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from 'react';
 import React from "react";
 
 const profile = () => {
