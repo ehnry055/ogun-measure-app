6d86d7d49cac935a34557c672ef6591a5ead4b05
dean1210e <121178947+dean1210e@users.noreply.github.com>
2025-12-19 12:59:47 -0500

    Revert "homepage jsx and css"
    This reverts commit 1a74b5275920b9368629e90fdce2e8354451af54.

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,49 +1,28 @@
-import React from "react";
-import "../styles/HomePage.css";
-import ogunImage from "../assets/OGUN.jpg";
+import React from 'react';
+import '../styles/HomePage.css'; 
 
 const HomePage = () => {
   return (
     <div className="home-container">
-      {/* HERO / HEADER */}
-      <header className="home-hero">
-        <div className="home-hero-left">
-          <img
-            src={ogunImage}
-            alt="Ogun Measure"
-            className="home-hero-image"
-          />
+      <h2 className="about-title">The Story</h2>
+      <div className="about-content">
+        <p>
+        In 2023, after developing and testing previous models of anti-Black structural racism,
+Dr. Paris B. Adkins-Jackson developed the Multifaceted Life Course Measure of Anti-
+Black Structural Racism, also referred to as the Ogun Measure. In close collaboration
+with Drs. Justina F. Avila-Rieger and Tanisha G. Hill-Jarrett, the Ogun measure was
+refined over the course of one year and presented to collaborators that would
+become item developers (see below). In 2024, under the management of Ms. Muriel
+Taks Calle, the Ogun measure underwent a rigorous data quality assurance process.
+In 2025, the measure was psychometrically validated and provided to the scientific
+community at-large. For more information on this measure, please read the manual
+and the seminal article. 
+        </p>
+        <div class="container">
+        <a href="/gateway" className="homebtn"><span> See Measure </span></a>
+        <a href="/itemdevelopers" className="homebtn"><span> See Item Developers </span></a>
         </div>
-
-        <div className="home-hero-right">
-          <h1>The Story</h1>
-          <p>
-            In 2023, after developing and testing previous models of anti-Black
-            structural racism, Dr. Paris B. Adkins-Jackson developed the
-            Multifaceted Life Course Measure of Anti-Black Structural Racism,
-            also referred to as the Ogun Measure. In close collaboration with
-            Drs. Justina F. Avila-Rieger and Tanisha G. Hill-Jarrett, the Ogun
-            measure was refined over the course of one year and presented to
-            collaborators that would become item developers.
-          </p>
-
-          <p>
-            In 2024, under the management of Ms. Muriel Taks Calle, the Ogun
-            measure underwent a rigorous data quality assurance process. In
-            2025, the measure was psychometrically validated and provided to the
-            scientific community at-large.
-          </p>
-
-          <div className="home-button-row">
-            <a href="/measure" className="home-button primary">
-              See Measure
-            </a>
-            <a href="/item-developers" className="home-button secondary">
-              See Item Developers
-            </a>
-          </div>
-        </div>
-      </header>
+      </div>
     </div>
   );
 };
