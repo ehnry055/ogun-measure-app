cea9069390145c938507192e4713d2e4ced8f283
dean1210e <121178947+dean1210e@users.noreply.github.com>
2026-01-06 11:34:10 -0500

    navbar changes

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,189 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import '../styles/Navbar.css'; 
-import '../styles/LoginLogout.css'; 
-import { useAuth0 } from '@auth0/auth0-react';
-import LoginButton from './login';
-import LogoutButton from './logout';
-import { Dropdown } from 'react-bootstrap'; // Add this import
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JS bundle
-import { jwtDecode } from 'jwt-decode';
-
-
-//CURRENT BUG: the log out button disappears when navigating to another page, i think isauthenticated isn't updated every time? confusing
+import "../styles/Navbar.css";
+import columbiaLogo from "../assets/columbia-logo.svg"; // add your logo here
 
 function Navbar() {
-  const { isLoading, error, user, loginWithRedirect, logout, getAccessTokenSilently, isAuthenticated } = useAuth0();
-  //AdminRole check
-  const [isAuthorized, setIsAuthorized] = useState(() => {
-    const initialState = false;
-    return initialState;
-  });
-  useEffect(() => {
-    const checkPermissions = async () => {
-        try {
-          const token = await getAccessTokenSilently();
-          console.log("Access token:", token); // Log the token for debugging
-          const decodedToken = jwtDecode(token);
-          console.log("Decoded token:", decodedToken);
-  
-          const hasPermission = decodedToken.permissions && decodedToken.permissions.includes("adminView");
-          console.log("Has permission:", hasPermission);
-  
-          if (!hasPermission) {
-          }
-          else {
-            setIsAuthorized(true);
-          }
-        } catch (error) {
-          console.error('Error checking permissions:', error);
-        }
-      };
-  
-      checkPermissions();
-    }, [isAuthenticated, getAccessTokenSilently]);
-
-
   return (
-    <nav class="navbar navbar-expand-lg navbar-light navbar-gradient sticky-top">
-      <div className="container-fluid">
-      <a class="navbar-brand" href="/">Ogun Database</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="/">Home {/* <span class="sr-only">(current)</span>*/}</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/gateway">Measure</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/viewdata">View Data</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/changedata">Upload/Delete Data</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/map">Map Display</a>
-          </li>
-          {/*
-          <li class="nav-item">
-            <a class="nav-link" href="/graphs">Graphs</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/users">Users</a>
-          </li>
-          */}
-          {/* <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li> */}
-        </ul>
-      {/*<form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> 
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> 
-      </form> */}
-      <div className="navbar-nav ms-auto" style={{minHeight: '50px'}}>
-        <div className="tba">
-          {error && <p>Authentication Error</p>}
-          {!error && isLoading &&  <p>Loading...</p>}  
-          {!error && !isLoading && (
-            <>
-              <LoginButton/>
-              {/* <LogoutButton/> */}
-            </>
-          )}
-          
-          {/* If logged in, show profile icon with dropdown */}
-          {!error && !isLoading && user && (
-                <Dropdown alignRight>
-                  <Dropdown.Toggle variant="success" id="dropdown-profile" className="custom-dropdown-toggle">
-                    <img
-                      src={user.picture}
-                      alt="Profile"
-                      className="rounded-circle"
-                      width="30"
-                      height="30"
-                    />
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu className="custom-dropdown-menu">
-                    <Dropdown.Item href="/profile" className="custom-dropdown-item">Profile</Dropdown.Item>
-                    <Dropdown.Item href="/requests" className="custom-dropdown-item">Requests</Dropdown.Item>
-                    {/*}
-                    <Dropdown.Item href="/settings" className="custom-dropdown-item">Settings</Dropdown.Item>
-                    */}
-                    {isAuthorized && (
-                      <Dropdown.Item href="/users" className="custom-dropdown-item">User Management</Dropdown.Item>
-                    )}
-                    <Dropdown.Item className="custom-dropdown-item" onClick={() => logout({ returnTo: window.location.origin })}>Logout</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              )}
+    <header className="navbar">
+      <div className="navbar-inner">
+        {/* Left: Columbia logo */}
+        <div className="navbar-logo">
+          <img
+            src={columbiaLogo}
+            alt="Columbia University"
+          />
         </div>
-      </div>
-      </div>
-    </div>
-  </nav>
 
-  );
-};
-    
-    
-    
-    
-    
-    
-    
-  {/** 
-    <div className="top-navbar">
-     <div className="navbar-left">
-       <img
-          src=
-          alt="Logo"
-          className="logo"
-        />
-        <span className="title">Placeholder</span>
-        <span className="subtitle">Racism Data System</span>
-      </div>
-      <div className="navbar-center">
-        <nav className="nav-links">
-          <a href="/" className="nav-link">🏠 Home</a>
-          <a href="/users" className="nav-link">👤 Users</a>
-          <a href="/data" className="nav-link">📁 Data</a>
-          <a href="/graphs" className="nav-link">📊 Graphs</a>
+        {/* Centered navigation */}
+        <nav className="navbar-nav">
+          <ul className="navbar-links">
+            <li><a href="/">Home</a></li>
+            <li><a href="/measure">Measure</a></li>
+            <li><a href="/data">Data</a></li>
+            <li><a href="/about">About</a></li>
+          </ul>
         </nav>
       </div>
-      <div className="navbar-right">
-        <input type="text" className="search-bar" placeholder="Search..." />
-        <div className="tba">
-          {error && <p>Authentication Error</p>}
-          {!error && isLoading &&  <p>Loading</p>}  
-           loading is a placeholder, replace with an animation! 
-          {!error && !isLoading && (
-            <>
-              <LoginButton />
-              <LogoutButton />
-            </>
-          )}
-        </div>
-      </div>
-    </div>
-        */}
-
-
+    </header>
+  );
+}
 
 export default Navbar;
