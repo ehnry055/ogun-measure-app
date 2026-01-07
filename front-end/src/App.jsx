06f5015782c70bd9d97c0b3a0cb5c6d9f3d3b1a8
dean1210e <121178947+dean1210e@users.noreply.github.com>
2026-01-07 10:16:56 -0500

    Update App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
@@ -37,47 +37,45 @@ import './styles/App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Navbar />
+    <div className="app-container">
+      <Navbar />
 
-        <div className="main-layout">
-          <div className="content">
-            <Routes>
-              {/* Public */}
-              <Route path="/" element={<HomePage />} />
-              <Route path="/gateway" element={<Gateway />} />
-              <Route path="/unauthorized" element={<UnAuthorized />} />
-              <Route path="/itemdevelopers" element={<ItemDevelopers />} />
-              <Route path="/map" element={<MapPage />} />
+      <div className="main-layout">
+        <div className="content">
+          <Routes>
+            {/* Public */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/gateway" element={<Gateway />} />
+            <Route path="/unauthorized" element={<UnAuthorized />} />
+            <Route path="/itemdevelopers" element={<ItemDevelopers />} />
+            <Route path="/map" element={<MapPage />} />
 
-              {/* Protected */}
-              <Route path="/viewdata" element={<PrivateRoute Component={ViewDatabasePage} />} />
-              <Route path="/changedata" element={<PrivateRoute Component={ChangeDatabasePage} />} />
-              <Route path="/graphs" element={<PrivateRoute Component={VisualizeDataPage} />} />
-              <Route path="/profile" element={<PrivateRoute Component={ProfilePage} />} />
-              <Route path="/requests" element={<PrivateRoute Component={RequestsPage} />} />
-              <Route path="/ogun" element={<PrivateRoute Component={OgunMeasure} />} />
+            {/* Protected */}
+            <Route path="/viewdata" element={<PrivateRoute Component={ViewDatabasePage} />} />
+            <Route path="/changedata" element={<PrivateRoute Component={ChangeDatabasePage} />} />
+            <Route path="/graphs" element={<PrivateRoute Component={VisualizeDataPage} />} />
+            <Route path="/profile" element={<PrivateRoute Component={ProfilePage} />} />
+            <Route path="/requests" element={<PrivateRoute Component={RequestsPage} />} />
+            <Route path="/ogun" element={<PrivateRoute Component={OgunMeasure} />} />
 
-              {/* Admin */}
-              <Route path="/users" element={<AdminRoute Component={EditUsers} />} />
+            {/* Admin */}
+            <Route path="/users" element={<AdminRoute Component={EditUsers} />} />
 
-              {/* Facets */}
-              <Route path="/ResidentialSegregationGentrification" element={<RSG />} />
-              <Route path="/PropertyOrganization" element={<PO />} />
-              <Route path="/GovernmentRepresentation" element={<GR />} />
-              <Route path="/PolicingIncarceration" element={<PI />} />
-              <Route path="/IncomePoverty" element={<IP />} />
-              <Route path="/OccupationalSegregationUnemployment" element={<OSU />} />
-              <Route path="/HealthcareAccess" element={<HA />} />
-              <Route path="/HealthyFoodAccess" element={<HFA />} />
-              <Route path="/EnvironmentalPollution" element={<EP />} />
-              <Route path="/MediaMarketing" element={<MM />} />
-            </Routes>
-          </div>
+            {/* Facets */}
+            <Route path="/ResidentialSegregationGentrification" element={<RSG />} />
+            <Route path="/PropertyOrganization" element={<PO />} />
+            <Route path="/GovernmentRepresentation" element={<GR />} />
+            <Route path="/PolicingIncarceration" element={<PI />} />
+            <Route path="/IncomePoverty" element={<IP />} />
+            <Route path="/OccupationalSegregationUnemployment" element={<OSU />} />
+            <Route path="/HealthcareAccess" element={<HA />} />
+            <Route path="/HealthyFoodAccess" element={<HFA />} />
+            <Route path="/EnvironmentalPollution" element={<EP />} />
+            <Route path="/MediaMarketing" element={<MM />} />
+          </Routes>
         </div>
       </div>
-    </Router>
+    </div>
   );
 }
 
