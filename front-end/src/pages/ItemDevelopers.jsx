2430228752288b73b26561b6c24ff41bb7d17531
dean1210e <121178947+dean1210e@users.noreply.github.com>
2026-01-14 14:47:03 -0500

    Update ItemDevelopers.jsx

--- front-end/src/pages/ItemDevelopers.jsx
+++ front-end/src/pages/ItemDevelopers.jsx
@@ -2,29 +2,42 @@ import React from 'react';
 import '../styles/HomePage.css'; 
 
 function ItemDevelopers() {
-    return (
+  return (
     <div className="home-container">
-        <h2 className="about-title">Item Developers</h2>
-        <div className="about-content">
-            <p>
+
+      <h2 className="about-title">Item Developers</h2>
+
+      <div className="about-content">
+
+        {/* BLUE INFO BOX (same as Home page) */}
+        <div className="info-box">
+          <p>
             Item developers included: Justina Avila-Rieger, Tanisha Hill-Jarrett, Ariana Gobaud, Boeun
-Kim, Muriel Taks Calle, Victoria Joseph, Longyuan Gao, Woomy Michel, Dillon Sparks, Ellesse
-Akre, Ganesh Babulal, Tahlia Bragg, Leia Belt, Safiyyah M. Okoye, Tiffany N. Ford, Jennifer J.
-Manly, Rachel Hardeman, Kene Orakwue, Katherine Keyes, Dominika Seblova, Meies-Amor
-Matz, Kendra Sims, Mudia Uzzi, Deidra C. Crews, Arindam Bagga, Melissa D. Hladek, Lisa L.
-Barnes, Sydney Isabella Leung, Gina Wang, and Yixuan Ding.
-</p> <p>Scientific and technical support came from: Muriel Taks Calle, Victoria Joseph, Christina A.
-Mehranbod, Gabriella Solomon, Gilbert Gee, Roland J. Thorpe, Jr., Laura Samuel, Karen
-Bandeen-Roche, Sarah Szanton,, Zeyu Dong, Cailyn E. Clemons, Jaren L. Wyaco, Richard
-George, Lorraine Benn, Yao Zeng, Sydney Isabella Leung, Gina Wang, and Yixuan Ding.
-            </p>
+            Kim, Muriel Taks Calle, Victoria Joseph, Longyuan Gao, Woomy Michel, Dillon Sparks, Ellesse
+            Akre, Ganesh Babulal, Tahlia Bragg, Leia Belt, Safiyyah M. Okoye, Tiffany N. Ford, Jennifer J.
+            Manly, Rachel Hardeman, Kene Orakwue, Katherine Keyes, Dominika Seblova, Meies-Amor
+            Matz, Kendra Sims, Mudia Uzzi, Deidra C. Crews, Arindam Bagga, Melissa D. Hladek, Lisa L.
+            Barnes, Sydney Isabella Leung, Gina Wang, and Yixuan Ding.
+          </p>
+
+          <p>
+            Scientific and technical support came from: Muriel Taks Calle, Victoria Joseph, Christina A.
+            Mehranbod, Gabriella Solomon, Gilbert Gee, Roland J. Thorpe, Jr., Laura Samuel, Karen
+            Bandeen-Roche, Sarah Szanton, Zeyu Dong, Cailyn E. Clemons, Jaren L. Wyaco, Richard
+            George, Lorraine Benn, Yao Zeng, Sydney Isabella Leung, Gina Wang, and Yixuan Ding.
+          </p>
+        </div>
+
+        {/* BUTTONS */}
         <div className="container">
-        <a href="/gateway" className="homebtn"><span> See Measure </span></a>
-        <a href="/" className="homebtn"><span> Return Home </span></a>
+          <a href="/gateway" className="homebtn"><span> See Measure </span></a>
+          <a href="/" className="homebtn"><span> Return Home </span></a>
         </div>
-        </div>
+
+      </div>
+
     </div>
-    );
-};
+  );
+}
 
 export default ItemDevelopers;
