a043b61e83480096a867b401dab078033848e9ce
dean1210e <121178947+dean1210e@users.noreply.github.com>
2026-02-22 15:33:27 -0500

    homepagechanges

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -4,6 +4,12 @@ import heroImage from "../assets/encouraging.png";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  const measures = [
+    "Anti-American Indian and/or Alaskan Native Measures",
+    "Multifaceted Life Course Measure of Anti-Black Structural Racism (Ogun)",
+    "Anti-Latine Measures",
+  ];
+
   return (
     <div className="home-page">
 
@@ -19,35 +25,23 @@ const HomePage = () => {
         </div>
       </div>
 
-      {/* STORY BOX */}
+      {/* MEASURES */}
       <div className="home-container">
-        <div className="story-box">
-          <h2 className="about-title">The Story</h2>
+        <div className="measures-box">
+          <h2 className="about-title">Measures</h2>
 
-          <div className="about-content">
-            <p>
-              In 2023, after developing and testing previous models of anti-Black structural racism,
-              Dr. Paris B. Adkins-Jackson developed the Multifaceted Life Course Measure of Anti-
-              Black Structural Racism, also referred to as the Ogun Measure. In close collaboration
-              with Drs. Justina F. Avila-Rieger and Tanisha G. Hill-Jarrett, the Ogun measure was
-              refined over the course of one year and presented to collaborators that would
-              become item developers (see below). In 2024, under the management of Ms. Muriel
-              Taks Calle, the Ogun measure underwent a rigorous data quality assurance process.
-              In 2025, the measure was psychometrically validated and provided to the scientific
-              community at-large. For more information on this measure, please read the manual
-              and the seminal article.
-            </p>
-
-            <div className="home-actions">
-              <Link to="/gateway" className="homebtn">
-                <span>See Measure</span>
-              </Link>
-
-              <Link to="/itemdevelopers" className="homebtn">
-                <span>See Item Developers</span>
-              </Link>
-            </div>
+          <div className="measure-grid">
+            {measures.map((title) => (
+              <div className="measure-card" key={title}>
+                <h3 className="measure-title">{title}</h3>
+                <Link to="/gateway" className="homebtn">
+                  <span>See Measure</span>
+                </Link>
+              </div>
+            ))}
           </div>
+          
+          <p className="about-content">Select a measure to continue.</p>
         </div>
       </div>
 
