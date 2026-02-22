bd455f9c549e3159c9e974afdd6bd9cbce7fac8b
dean1210e <121178947+dean1210e@users.noreply.github.com>
2026-02-22 15:41:26 -0500

    Update HomePage.jsx

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -18,9 +18,12 @@ const HomePage = () => {
         <img src={heroImage} alt="Encouraging" />
 
         <div className="hero-overlay">
-          <h1 className="hero-main-title">Ogun Measure</h1>
+          <h1 className="hero-main-title">Measures of Structural Racism</h1>
           <p className="hero-subtitle">
-            A multifaceted life course measure of anti-Black structural racism
+            To change the future, we must reckon with the past.
+            To heal people, we must change the circumstances that produce disease.
+            These measures of structural racism are as artifacts of stories that affect the present. 
+            Use them to transform trauma and pain into health.
           </p>
         </div>
       </div>
