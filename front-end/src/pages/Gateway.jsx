7677c860d70ff02b62b1e55592a1d3ce4383a630
dean1210e <121178947+dean1210e@users.noreply.github.com>
2026-04-13 11:51:54 -0400

    Update Gateway.jsx

--- front-end/src/pages/Gateway.jsx
+++ front-end/src/pages/Gateway.jsx
@@ -6,9 +6,6 @@ import { Dropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import OGUN from "../assets/OGUN.jpg";
-import Headdress from "../assets/Headdress.jpg";
-import alaska from "../assets/alaska.jpg";
-
 
 const Gateway = () => {
   const location = useLocation();
@@ -32,7 +29,9 @@ const Gateway = () => {
       
       <div id="anti-indigenous" className="gateway-split">
         <div className="gateway-left">
-          <img src={alaska} alt="Alaska" className="ogunimg-full" />
+          <div style={{ width: '100%', height: '100%', backgroundColor: '#e0e0e0', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#666' }}>
+             Placeholder Image
+          </div>
         </div>
         <div className="gateway-right">
           <h2 className="about-title gateway-title">Anti-American Indian / Alaskan Native Measures</h2>
@@ -99,7 +98,9 @@ const Gateway = () => {
 
       <div id="anti-latine" className="gateway-split">
         <div className="gateway-left">
-          <img src={Headdress} alt="Headdress" className="ogunimg-full" />
+          <div style={{ width: '100%', height: '100%', backgroundColor: '#e0e0e0', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#666' }}>
+             Placeholder Image
+          </div>
         </div>
         <div className="gateway-right">
           <h2 className="about-title gateway-title">Anti-Latine Measures</h2>
